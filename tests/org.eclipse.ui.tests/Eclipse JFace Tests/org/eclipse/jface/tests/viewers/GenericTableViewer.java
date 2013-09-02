1e209003d1f8177dd265e7b7d2e467bd5b85eb2e
Hendrik Still <hendrik.still@gammas.de>
2013-09-02 23:21:44 +0200

    Added JUnit Test

---
+++ tests/org.eclipse.ui.tests/Eclipse JFace Tests/org/eclipse/jface/tests/viewers/GenericTableViewer.java
@@ -0,0 +1,94 @@
+/*******************************************************************************
+ * Copyright (c) 2013 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ ******************************************************************************/
+
+package org.eclipse.jface.tests.viewers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.eclipse.jface.viewers.IStructuredContentProvider;
+import org.eclipse.jface.viewers.StructuredViewer;
+import org.eclipse.jface.viewers.TableViewer;
+import org.eclipse.jface.viewers.Viewer;
+import org.eclipse.swt.widgets.Composite;
+import org.junit.Test;
+
+/**
+ * @since 3.5
+ *
+ */
+public class GenericTableViewer extends ViewerTestCase{
+
+	private TableViewer<TestModel, List<TestModel>> tableViewer;
+
+	/**
+	 * @param name
+	 */
+	public GenericTableViewer(String name) {
+		super(name);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Test
+	public void test() {
+
+	}
+
+	/* (non-Javadoc)
+	 * @see org.eclipse.jface.tests.viewers.ViewerTestCase#createViewer(org.eclipse.swt.widgets.Composite)
+	 */
+	@Override
+	protected StructuredViewer createViewer(Composite parent) {
+		tableViewer = new TableViewer<TestModel, List<TestModel>>(parent);
+		tableViewer.setContentProvider(new IStructuredContentProvider<TestModel, List<TestModel>>() {
+
+			public void dispose() {
+				//Nothing here
+			}
+
+			public void inputChanged(Viewer<List<TestModel>> viewer,
+					List<TestModel> oldInput, List<TestModel> newInput) {
+				//Nothing here
+			}
+
+			public TestModel[] getElements(List<TestModel> inputElement) {
+				return inputElement.toArray(new TestModel[inputElement.size()]);
+			}
+		});
+
+		return tableViewer;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.eclipse.jface.tests.viewers.ViewerTestCase#setInput()
+	 */
+	@Override
+	protected void setInput() {
+		List<TestModel> testModels = new ArrayList<TestModel>();
+		testModels.add(new TestModel(0, 0));
+		testModels.add(new TestModel(0, 0));
+		testModels.add(new TestModel(0, 0));
+		testModels.add(new TestModel(0, 0));
+		tableViewer.setInput(testModels);
+	}
+
+	class SubTestModel extends TestModel{
+
+		/**
+		 * @param numLevels
+		 * @param numChildren
+		 */
+		public SubTestModel(int numLevels, int numChildren) {
+			super(numLevels, numChildren);
+		}
+
+	}
+}
